4077a4db2e4ac7803dbb516ea0645b1369ef2383
Jesus Brito <email@jesusbrito.dev>
2026-07-10 12:40:27 +0200

    Filter fixed

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,26 +1,30 @@
 const filtersContainer = document.getElementById("filters");
 const filters = filtersContainer?.querySelectorAll('select');
 
-let filterName = " ";
-let value = " ";
+        
+const jobsListing = document.querySelector(".job-listings");
+const articles = jobsListing?.querySelectorAll("article")
+
+let filterName = "";
+let filterValue = "";
 
 filters?.forEach(filter =>{
     filter.addEventListener('change', function(event){
+
         filterName = event.target.name;
-        value = event.target.value;
-        console.log(filterName + " " + value);
+        filterValue = event.target.value;
+        console.log(filterName + " " + filterValue);
+
+        articles?.forEach(article => {
+
+            const contrato = article.querySelector(".contrato").textContent.trim();
+            
+            if(contrato === filterValue ){
+                article.classList.remove("hidden");
+            }else{
+                article.classList.add("hidden");
+            }
+        });
     })
 })
 
-
-
-const jobsListing = document.querySelector(".job-listings");
-const articles = jobsListing.querySelectorAll("article")
-
-articles.forEach(article => {
-    if(article.querySelector(".contrato") === value ){
-        article.classList.remove("hidden");
-    }else{
-        article.classList.add("hidden");
-    }
-});
